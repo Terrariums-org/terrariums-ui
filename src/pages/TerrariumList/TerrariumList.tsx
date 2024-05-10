28f0aa2e007ccca79a3e5f85f5e689d83678ece5
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-05-10 13:27:02 -0600

    img art added to terrariumPage and layer done

--- src/pages/TerrariumList/TerrariumList.tsx
+++ src/pages/TerrariumList/TerrariumList.tsx
@@ -1,6 +1,8 @@
 import styles from "./TerrariumList.module.css";
 import { HomeLayer } from "../../components/HomeLayer/HomeLayer";
 import { HeaderList } from "./components/HeaderList/HeaderList";
+import GeckoImg from "../../assets/imgs/gecko.png";
+import { ArtImg } from "../../components/ArtImg/ArtImg";
 
 export const TerrariumList = () => {
   // create 2 components for dashboard and add register
@@ -10,6 +12,10 @@ export const TerrariumList = () => {
   return (
     <HomeLayer>
       <HeaderList />
+      <div className={styles.containerContent}>
+        <div></div>
+        <ArtImg urlImg={GeckoImg} altImg="Gecko Img" />
+      </div>
     </HomeLayer>
   );
 };
